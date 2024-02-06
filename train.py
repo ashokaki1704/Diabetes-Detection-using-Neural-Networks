80c05a852bc2e2120ee7456d602e8d564a6b9b06
Naga Venkata Ashok Aki <153583079+ashokaki1704@users.noreply.github.com>
2024-02-06 19:18:03 +0530

    Add files via upload

---
+++ train.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from numpy import loadtxt #handle/load dataset
+from keras.models import Sequential #Empty working area 
+from keras.layers import Dense #Dense layer 
+from sklearn.metrics import accuracy_score
+csv_file_path= r"C:\Users\91939\Desktop\deep learning\8\8\pima-indians-diabetes.csv"
+dataset=pd.read_csv(csv_file_path)
+x = dataset[:,0:8]
+y = dataset[:,8]
+print(x)
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(x, y, epochs=200, batch_size=10)
+_, accuracy = model.evaluate(x, y)
+print('Accuracy: %.2f' % (accuracy*100))
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+model.save_weights("model.h5")
+print("Saved model to disk")
